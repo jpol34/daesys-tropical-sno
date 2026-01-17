10b48cb72906940971f854967f28ca347a5b206d
Jordan Polyak <jordan.polyak@gmail.com>
2026-01-16 22:59:37 -0600

    Add custom SVG favicon with sno cone design

--- src/lib/data/businessInfo.ts
+++ src/lib/data/businessInfo.ts
@@ -22,6 +22,10 @@ export const businessInfo = {
 	googleMapsUrl: 'https://www.google.com/maps/search/?api=1&query=3814+Little+Rd+Arlington+TX+76016',
 	googleMapsEmbed: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3356.5!2d-97.15!3d32.73!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMzLCsDQzJzQ4LjAiTiA5N8KwMDknMDAuMCJX!5e0!3m2!1sen!2sus!4v1234567890',
 	social: {
+		x: {
+			handle: '@daesystropicalsno',
+			url: 'https://x.com/daesystropicalsno'
+		},
 		instagram: {
 			handle: '@daesystropicalsno',
 			url: 'https://www.instagram.com/daesystropicalsno'
